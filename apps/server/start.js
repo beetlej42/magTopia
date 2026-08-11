6df4bb29effd52b3f7c1b0eadd1a2adcd8521340
beetlej42 <128801891+beetlej42@users.noreply.github.com>
2026-08-11 19:04:23 +0800

    install persistent player sessions

--- apps/server/start.js
+++ apps/server/start.js
@@ -3,6 +3,7 @@ import { loadConfig } from "./config.js";
 import { createDatabase, migrateDatabase } from "./database.js";
 import { createRepository } from "./repository.js";
 import { createWorker } from "./worker.js";
+import { installPlayerSessions } from "./player-session.js";
 
 const config = loadConfig();
 const database = createDatabase(config.databaseUrl);
@@ -10,13 +11,14 @@ if (config.autoMigrate) await migrateDatabase(database);
 const repository = createRepository(database, config);
 await repository.seedBuiltinAssets();
 const app = await createApp({ repository, config, logger: true });
+installPlayerSessions({ app, repository, config });
 const worker = createWorker({ repository, config });
 
 await app.listen({ host: config.host, port: config.port });
 worker.run().catch((error) => app.log.error(error));
 
 async function shutdown(signal) {
-    app.log.info({ signal }, "Shutting down MAGTOPIA");
+  app.log.info({ signal }, "Shutting down MAGTOPIA");
   worker.stop();
   await app.close();
   await database.close();
